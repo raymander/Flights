b6987050030bf358a5b3da541076f29a282508bf
raymander <raychern@yandex.com>
2018-10-04 21:32:40 +0300

    add test

--- flights/admin.py
+++ flights/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Airport, Flight, Passenger
 # Register your models here.
 
+# StackedInline - built-in class that add new relationships between objects
+class PassengerInline(admin.StackedInline):
+    #through tells us that there is an intermediary table between passengers and flights
+    model = Passenger.flights.through
+    extra = 1
+
+class FlightAdmin(admin.ModelAdmin):
+    inlines = [PassengerInline]
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ('flights',)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
